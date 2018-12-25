3c9eb31f666362b2dd50c84343976ed12abf3a51
RyukiMizoguchi <mgr31028@gmail.com>
2018-12-25 11:28:22 +0900

    modigy indent

--- practice.rb
+++ practice.rb
@@ -1,12 +1,12 @@
 describe "四則演算" do
-    it "1+1は2になること" do
-        expect(1+1).to eq 2
-    end
+  it "1+1は2になること" do
+    expect(1+1).to eq 2
+  end
 end
 
 describe "四則演算" do
 	it "10 - 1 は 9 になること" do
-		expect(10 - 1).to eq 9
+	  expect(10 - 1).to eq 9
 	end
 end
 
@@ -63,20 +63,20 @@ end
 #context で条件別にグループ化する
 
 describe User do
-    describe '#greet' do
-      context '12歳以下の場合' do
-        it 'ひらがなで答えること' do
-          user = User.new(name: 'たろう', age: 12)
-          expect(user.greet).to eq 'ぼくはたろうだよ。'
-        end
-      end
-      context '13歳以上の場合' do
-        it '漢字で答えること' do
-          user = User.new(name: 'たろう', age: 13)
-          expect(user.greet).to eq '僕はたろうです。'
-        end
+  describe '#greet' do
+    context '12歳以下の場合' do
+      it 'ひらがなで答えること' do
+        user = User.new(name: 'たろう', age: 12)
+        expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
+    context '13歳以上の場合' do
+      it '漢字で答えること' do
+        user = User.new(name: 'たろう', age: 13)
+        expect(user.greet).to eq '僕はたろうです。'
+      end
+    end
+  end
 end
 
 
